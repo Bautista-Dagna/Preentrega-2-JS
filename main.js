852d8c3a2831be8056b6c116f7773ef7af53cdf9
Bautista <bautidagna@gmail.com>
2023-05-16 20:21:05 -0300

    preentregajscorregido

--- main.js
+++ main.js
@@ -1,74 +1,81 @@
 const hamburguesas = [
-     { nombre: "Pipa Tasty",
-      precio: 1350
-    },
-     { nombre: "Big Pipa",
-     precio: 1300
-    },
-    { nombre: "Pipo Burger",
-      precio: 1400
-    },
-    { nombre: "Cuarto de Pipa",
-      precio: 1250
-    },
-];;
+  { nombre: "Pipa Tasty", precio: 1350 },
+  { nombre: "Big Pipa", precio: 1300 },
+  { nombre: "Pipo Burger", precio: 1400 },
+  { nombre: "Cuarto de Pipa", precio: 1250 },
+];
 
-let carrito = [ ]
+let carrito = [];
 
-let seleccion = prompt ("Bienvenido a de la Pipa, ¿Ya decidió qué va a pedir?")
-while (seleccion != "si" && seleccion  != "no") {   
-    alert ("por favor ingrese si o no")
-    seleccion = prompt("Vamos de nuevo, ¿ Ya decidiste que vas a comer hoy?")
+let seleccion = prompt("Bienvenido a de la Pipa, ¿Ya decidió qué va a pedir?");
+while (seleccion !== "si" && seleccion !== "no") {
+  alert("Por favor ingrese si o no");
+  seleccion = prompt("Vamos de nuevo, ¿Ya decidiste qué vas a comer hoy?");
 }
 
-if(seleccion == "si"){
-alert ("Por favor diga cuál de estas opciones le convece más")
-//console.log (hamburguesas)
-let mostrarHamburguesas = hamburguesas.map((hamburguesas) => hamburguesas.nombre + " " + hamburguesas.precio + "$")
-alert(mostrarHamburguesas.join ( " - " ))
-} else if (seleccion == "no") {
-    alert("Gracias por visitarnos, te esperamos la proxima!")
+if (seleccion === "si") {
+  alert("Por favor diga cuál de estas opciones le convence más");
+  let mostrarHamburguesas = hamburguesas.map(
+    (hamburguesa) => `${hamburguesa.nombre} ${hamburguesa.precio}$`
+  );
+  alert(mostrarHamburguesas.join(" - "));
+} else if (seleccion === "no") {
+  alert("Gracias por visitarnos, ¡te esperamos la próxima vez!");
 }
 
-while (seleccion != "no"){
-    let producto = prompt ("agregue el producto deseado al carrito")
-let precio = 0
+while (seleccion !== "no") {
+  let producto = prompt("Agregue el producto deseado al carrito");
+  let precio = 0;
 
-
-if(producto == "Pipa Tasty" || "Big Pipa" || "Pipo Burguer" || "Cuarto de Pipa"  ) {
-    switch(producto) {
-        case "Pipa Tasty" :
-        precio = 1350
+  if (
+    producto === "Pipa Tasty" ||
+    producto === "Big Pipa" ||
+    producto === "Pipo Burger" ||
+    producto === "Cuarto de Pipa"
+  ) {
+    switch (producto) {
+      case "Pipa Tasty":
+        precio = 1350;
         break;
-case "Big Pipa":
-precio = 1300
-case "Pipo Burguer" :
-        precio = 1400
+      case "Big Pipa":
+        precio = 1300;
         break;
- case "Cuarto de Pipa" :
-        precio = 1250
+      case "Pipo Burger":
+        precio = 1400;
         break;
-default:
-    break;
+      case "Cuarto de Pipa":
+        precio = 1250;
+        break;
+      default:
+        break;
     }
-let unidades = parseInt(prompt("¿cuantas unidades desea de la misma?"))
 
-carrito.push ({producto, unidades, precio})
-console.log(carrito)
+    let unidades = parseInt(prompt("¿Cuántas unidades desea del mismo producto?"));
 
-  } else {alert("El dato ingresado no es correcto")}
+    carrito.push({ producto, unidades, precio });
+    console.log(carrito);
+  } else {
+    alert("El dato ingresado no es correcto");
+  }
 
-  seleccion = prompt ("¿Desea agregar algo más?")
-  while(seleccion == "no"){
-    alert("Gracias por su compra!")
-    carrito.forEach((carritoFinal) => {
-        console.log(`producto: ${carritoFinal.producto}, unidades: ${carritoFinal.unidades},
-        total a pagar por producto: ${carritoFinal.unidades * carritoFinal.precio}`)
-    })
-    break;
+  seleccion = prompt("¿Desea agregar algo más?");
+  while (seleccion !== "si" && seleccion !== "no") {
+    alert("Por favor ingrese si o no");
+    seleccion = prompt("¿Desea agregar algo más?");
   }
 }
 
-const total = carrito.reduce((acumulador, elementos) => acumulador + elementos.precio * elementos.unidades, 0)
-alert(`El total a pagar es de: ${total}`)
+alert("Gracias por su compra!");
 
+carrito.forEach((carritoFinal) => {
+  console.log(
+    `Producto: ${carritoFinal.producto}, Unidades: ${carritoFinal.unidades},
+    Total a pagar por producto: ${carritoFinal.unidades * carritoFinal.precio}`
+  );
+});
+
+const total = carrito.reduce(
+  (acumulador, elemento) => acumulador + elemento.precio * elemento.unidades,
+  0
+);
+alert(`El total a pagar es de: ${total}`);
